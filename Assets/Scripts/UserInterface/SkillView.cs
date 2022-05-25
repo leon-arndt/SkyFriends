bb370bcda616c24d3cb8392b6078425ba43f2b65
Leon Arndt <neotalon27@gmail.com>
2022-05-25 22:29:56 +0200

    move UI to its own scripts folder

---
+++ Assets/Scripts/UserInterface/SkillView.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+namespace UserInterface
+{
+    public class SkillView : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI level;
+
+        public void Display(KeyValuePair<SkillType, SkillData> skill)
+        {
+            level.text = skill.Value.level.ToString();
+        }
+    }
+}
---
+++ Assets/Scripts/UserInterface/SkillsView.cs
@@ -0,0 +1,36 @@
+﻿using Events;
+using UniRx;
+using UnityEngine;
+
+namespace UserInterface
+{
+    public class SkillsView : MonoBehaviour
+    {
+        [SerializeField] private RectTransform holder;
+        [SerializeField] private SkillView skillViewPrefab;
+        [SerializeField] private SkillSystem skillSystem;
+
+        private void Start()
+        {
+            DisplayCurrentSkills();
+            MessageBroker.Default
+                .Receive<SkillChanged>()
+                .TakeUntilDestroy(this)
+                .Subscribe(_ => DisplayCurrentSkills());
+        }
+
+        private void DisplayCurrentSkills()
+        {
+            foreach (var child in holder.gameObject.GetComponentsInChildren<SkillView>())
+            {
+                Destroy(child.gameObject);
+            }
+
+            foreach (var skill in skillSystem.GetAll())
+            {
+                var skillView = Instantiate(skillViewPrefab, holder);
+                skillView.Display(skill);
+            }
+        }
+    }
+}
