7302558d3d7ff9dd6cb570647dcfd478f1154606
Leon Arndt <neotalon27@gmail.com>
2022-05-26 09:41:10 +0200

    Lock mouse while playing

---
+++ Assets/Scripts/InputCamera.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class InputCamera : MonoBehaviour
+{
+    private void Start()
+    {
+        LockMouse();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            LockMouse();
+        }
+    }
+
+    private static void LockMouse()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+}
