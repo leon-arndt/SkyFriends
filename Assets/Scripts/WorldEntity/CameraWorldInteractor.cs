a777e5b7154f3424357c68467d849d1c781bd5c4
Leon Arndt <neotalon27@gmail.com>
2022-05-27 18:16:55 +0200

    move each script to a scripts subfolder

--- Assets/Scripts/WorldEntity/CameraWorldInteractor.cs
+++ Assets/Scripts/WorldEntity/CameraWorldInteractor.cs
@@ -6,12 +6,8 @@ public class CameraWorldInteractor : MonoBehaviour
 {
     [SerializeField] private SkillSystem skillSystem;
     
-    private Camera _camera;
-
     private void Start()
     {
-        _camera = Camera.main;
-
         Observable.Interval(TimeSpan.FromSeconds(10f))
             .TakeUntilDestroy(this)
             .Subscribe(_ => skillSystem.Gain(SkillType.Patience, 5));
