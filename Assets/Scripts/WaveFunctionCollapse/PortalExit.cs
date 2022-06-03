c5cac2e34d8877ab641029485a1ce2d104738dfe
Leon Arndt <neotalon27@gmail.com>
2022-06-03 22:51:29 +0200

    add PortalExit level feature

---
+++ Assets/Scripts/WaveFunctionCollapse/PortalExit.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using WorldEntity;
+
+namespace WaveFunctionCollapse
+{
+    [CreateAssetMenu(menuName = nameof(PortalExit))]
+    public class PortalExit : ScriptableObject, ILevelGeneratorFeature
+    {
+        [SerializeField] private Transform prefab;
+        
+        public void Handle(IOverlapWfcInfo levelLayer)
+        {
+            var portals = FindObjectsOfType<Portal>();
+
+            foreach (var portal in portals)
+            {
+                if (portal.target != null) continue;
+                var uniquePrefab = Instantiate(prefab, levelLayer.Transform);
+                uniquePrefab.gameObject.transform.localPosition = new Vector3(
+                    Random.Range(0, levelLayer.Size.x),
+                    1,
+                    -Random.Range(0, levelLayer.Size.y)
+                );
+                    
+                portal.target = uniquePrefab;
+                return;
+            }
+        }
+    }
+}
