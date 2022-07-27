11dce0611c38f97faecfd1ebc0d625ab4b9dce16
Leon Arndt <neotalon27@gmail.com>
2022-07-27 19:58:04 +0200

    animate player stat bars on change

--- Assets/Scripts/UserInterface/Overlay/StatsView.cs
+++ Assets/Scripts/UserInterface/Overlay/StatsView.cs
@@ -2,6 +2,7 @@
 using ScriptableObjectSystems;
 using UniRx;
 using UnityEngine;
+using Utility;
 
 namespace UserInterface.Overlay
 {
@@ -9,7 +10,7 @@ namespace UserInterface.Overlay
     {
         [SerializeField] private StatSystem statSystem;
         [SerializeField] private TextMeshSlider health, energy;
-        
+
         private void Start()
         {
             DisplayCurrentStats();
@@ -18,16 +19,18 @@ namespace UserInterface.Overlay
                 .TakeUntilDestroy(this)
                 .Subscribe(_ => DisplayCurrentStats());
         }
-        
+
         private void DisplayCurrentStats()
         {
             var currentHealth = statSystem.Get(StatType.Health).amount;
             var maxHealth = statSystem.Get(StatType.MaxHealth).amount;
             health.Set((float)currentHealth / maxHealth, $"{currentHealth}/{maxHealth}");
-            
+            health.transform.Shake();
+
             var currentCharge = statSystem.Get(StatType.UltimateCharge).amount;
             var maxCharge = statSystem.Get(StatType.MaxUltimateCharge).amount;
             energy.Set((float)currentCharge / maxCharge, $"{currentCharge}/{maxCharge}");
+            health.transform.Shake();
         }
     }
 }
