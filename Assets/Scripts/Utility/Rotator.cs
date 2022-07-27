57966555598f5e6b0f053a0eef63f9ced3314c4b
Leon Arndt <neotalon27@gmail.com>
2022-07-27 18:50:28 +0200

    rotate pickups and green world

---
+++ Assets/Scripts/Utility/Rotator.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+namespace Utility
+{
+	public class Rotator : MonoBehaviour
+	{
+		[SerializeField] private float speed = 1f;
+
+		private void Update()
+		{
+			transform.Rotate(new Vector3(0, 0, 45) * (Time.deltaTime * speed));
+		}
+	}
+}
