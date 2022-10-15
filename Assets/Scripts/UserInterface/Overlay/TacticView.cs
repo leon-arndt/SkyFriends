764e7f36ad2bcf826ee233a630774cc9add570a7
Leon Arndt <leon.arndt@innogames.com>
2022-10-15 18:45:47 +0200

    show image for current tactic

--- Assets/Scripts/UserInterface/Overlay/TacticView.cs
+++ Assets/Scripts/UserInterface/Overlay/TacticView.cs
@@ -1,27 +1,35 @@
-﻿using Events;
+﻿using System.Collections.Generic;
+using Events;
 using TMPro;
 using UniRx;
 using UnityEngine;
+using UnityEngine.UI;
 
-namespace UserInterface
+namespace UserInterface.Overlay
 {
-    public class TacticView : MonoBehaviour
-    {
-        [SerializeField] private ScriptableInt tacticSystem;
-        [SerializeField] private TextMeshProUGUI tacticName;
-        
-        private void Start()
-        {
-            UpdateView();
-            MessageBroker.Default
-                .Receive<TacticChanged>()
-                .TakeUntilDestroy(this)
-                .Subscribe(_ => UpdateView());
-        }
+	public class TacticView : MonoBehaviour
+	{
+		[SerializeField] private ScriptableInt tacticSystem;
+		[SerializeField] private TextMeshProUGUI tacticName;
+		[SerializeField] private Image tacticIcon;
+		[SerializeField] private Sprite[] tacticIcons;
 
-        private void UpdateView()
-        {
-            tacticName.text = ((Tactic) tacticSystem.value).ToString();
-        }
-    }
+		private void Start()
+		{
+			UpdateView();
+			MessageBroker.Default
+				.Receive<TacticChanged>()
+				.TakeUntilDestroy(this)
+				.Subscribe(_ => UpdateView());
+		}
+
+		private void UpdateView()
+		{
+			tacticName.text = ((Tactic) tacticSystem.value).ToString();
+			if (tacticSystem.value >= 0 && tacticSystem.value < tacticIcons.Length)
+			{
+				tacticIcon.sprite = tacticIcons[tacticSystem.value];
+			}
+		}
+	}
 }
