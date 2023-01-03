57ec1aa82160684ca204397bc4813236f625f15c
Leon Arndt <neotalon27@gmail.com>
2023-01-03 22:41:55 +0100

    walking animation

---
+++ Assets/Scripts/Utility/Walk.cs
@@ -0,0 +1,29 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+namespace Utility
+{
+	public class Walk : MonoBehaviour
+	{
+		[SerializeField] private float moveHeight = 0.1f;
+		[SerializeField] private float rotateAmount = 15f;
+
+		private Tween bounceTween, rotateTween;
+
+		private void Start()
+		{
+			const float bounceDuration = 0.15f;
+			bounceTween = transform
+				.DOLocalMoveY(moveHeight, bounceDuration)
+				.SetLoops(-1, LoopType.Yoyo)
+				.SetDelay(Random.value);
+
+			transform.Rotate(Vector3.forward * -0.5f * rotateAmount, Space.Self);
+			rotateTween = transform
+				.DOLocalRotate(Vector3.forward * rotateAmount, 1f)
+				.SetLoops(-1, LoopType.Yoyo)
+				.SetDelay(Random.value)
+				.SetEase(Ease.InOutSine);
+		}
+	}
+}
